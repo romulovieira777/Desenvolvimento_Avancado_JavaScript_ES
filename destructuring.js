078d4a9a342abfb267e9abdb0bef91b030b86bef
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-03 12:31:24 -0300

    Destructuring Assignment

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+var arr = ['Apple', 'Banana', 'Orange'];
+
+var apple = [0];
+var banana = [1];
+var orange = [2];
+
+// Destructuring Assignment
+
+var [Apple2, Banana2, Orange2] = ['Apple', 'Banana', 'Orange'];
+
+console.log(apple, apple2);
