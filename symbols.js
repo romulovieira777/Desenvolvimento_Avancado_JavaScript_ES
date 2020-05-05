1f48e2f55b8aefbd3ae78df7d399bbf516e97990
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-05 18:20:32 -0300

    Symbols

---
+++ symbols.js
@@ -0,0 +1,14 @@
+//Symbol
+
+const uniqueId = Symbol('Hello');
+
+console.log(uniqueId);
+
+// Well Known Symbols
+
+Symbol.iterator;
+Symbol.split;
+Symbol.toStringTag;
+
+const arr = [1, 2, 3, 4];
+const arr = 'Digitla Innovation One';
