b4362515cc0a9b64fa21212740a68ea88d75b240
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-12 17:13:40 -0300

    Assert

---
+++ chai.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const math = require('../src/math.js');
+
+let value = 0;
+
+describe('Math class', function () {
+    //hooks
+    beforeEach(function() {
+        value = 0;
+    })
+
+    it ('Sum two numbers', function (done) {
+        const math = new Math();
+        this.timeout(3000);
+
+        value = 5;
+
+        math.sum(value, 5, value => {
+            assert.equal(value, 10);
+            done();
+        });
+    });
+
+    it('Multiply two numbers', function() {
+        const math = new Math();
+
+        assert.equal(math.multiply(value, 5), 0);
+    });
+});
