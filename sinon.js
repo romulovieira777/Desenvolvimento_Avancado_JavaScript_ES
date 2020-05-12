762e40bda59579c6586564b8dd4d4d5cebba6f93
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-05-12 17:26:55 -0300

    Sinon

---
+++ sinon.js
@@ -0,0 +1,15 @@
+calss Math {
+    sum(a, b, callback) {
+        setTimeout(() => {
+            callback(a + b);
+        }, 2500);
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    printSum(req, res, a, b) {
+        res.load('index, a + b);
+    }
+}
+
+modulo.exports = Math;
